1e6102f36423ce5f87a0410792c05ebd9842086c
Kowshik <ks.dandavathi@gmail.com>
2025-05-13 08:24:52 -0400

    Stacks and Queues with methods

--- Base-Functions/Queues.js
+++ Base-Functions/Queues.js
@@ -1,1 +1,52 @@
-class Node {    constructor(val) {        this.val = val        this.next = null    }}class Queue {    constructor() {        this.first = null        this.last = null        this.size = 0    }    enqueue(val) {        if (val === null || val === undefined) return undefined        const node = new Node(val);        if (this.size === 0) {            this.first = node            this.last = node        } else {            this.last.next = node            this.last = node        }        this.size++;        return this;    }    dequeue() {        if (this.size === 0) return undefined        const removedNode = this.first;        if (this.size === 1) {            this.first = null            this.last = null        } else {            this.first = removedNode.next;            removedNode.next = null;        }        this.size--;        return removedNode;    }}const queue = new Queue();queue.enqueue("1")queue.enqueue("2")queue.enqueue("3")queue.enqueue("4")queue.dequeue()console.log(queue.dequeue())console.log(queue)
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+
+    enqueue(val) {
+        if (val === null || val === undefined) return undefined
+        const node = new Node(val);
+        if (this.size === 0) {
+            this.first = node
+            this.last = node
+        } else {
+            this.last.next = node
+            this.last = node
+        }
+        this.size++;
+        return this;
+    }
+
+    dequeue() {
+        if (this.size === 0) return undefined
+        const removedNode = this.first;
+        if (this.size === 1) {
+            this.first = null
+            this.last = null
+        } else {
+            this.first = removedNode.next;
+            removedNode.next = null;
+        }
+        this.size--;
+        return removedNode;
+    }
+}
+
+const queue = new Queue();
+queue.enqueue("1")
+queue.enqueue("2")
+queue.enqueue("3")
+queue.enqueue("4")
+queue.dequeue()
+console.log(queue.dequeue())
+
+console.log(queue)
