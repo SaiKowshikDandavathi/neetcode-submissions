484863a5ae965bc5231c35ac400d8866e078a459
Sai Kowshik Dandavathi <33039743+SaiKowshikDandavathi@users.noreply.github.com>
2026-07-16 20:22:23 -0400

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+
+        let lo = 0, high = numbers.length - 1;
+        while(lo < high){
+            if(numbers[lo] + numbers[high] === target) return [lo + 1, high + 1]
+            if(numbers[lo] + numbers[high] > target){
+                high--
+            } else {
+                lo++
+            }
+        }
+        return []
+    }
+}
