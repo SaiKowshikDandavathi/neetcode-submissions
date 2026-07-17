8680a6d9ddd44d66fdbc218984ba7732e8c6b2c0
Sai Kowshik Dandavathi <33039743+SaiKowshikDandavathi@users.noreply.github.com>
2026-07-17 14:13:06 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        let index = 0;
+        while (index < s.length) {
+            const char = s[index];
+            let prev;
+            if (char === "]" || char === "}" || char === ")") {
+                prev = stack.length ? stack.pop() : null;
+                if (!prev || prev !== mapping[char]) return false;
+            } else {
+                stack.push(char);
+            }
+            index++;
+        }
+        return !stack.length;
+    }
+}
+
+const mapping = {
+    "]": "[",
+    "}": "{",
+    ")": "(",
+};
